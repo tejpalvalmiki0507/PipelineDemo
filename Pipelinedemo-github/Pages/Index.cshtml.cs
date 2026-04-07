3cb2c2cb309036f1a35e084bb9392780b3468662
Tejpal Walmiki <twalmiki@hso.com>
2026-04-07 19:19:49 +0530

    Add project files.

---
+++ Pipelinedemo-github/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Pipelinedemo_github.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
